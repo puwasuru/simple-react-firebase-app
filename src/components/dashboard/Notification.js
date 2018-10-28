d3190d7d5f8b0479646ce90dfff23ed91e20dd7c
Puwasuru Ihalagedara <puwasuru95@gmail.com>
2018-10-28 12:36:08 +0530

    Notification components

--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Notification = () => {
+const Notification = (props) => {
     return(
-        <div>
-            <p>Notification</p>
+        <div className="secton">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className="notification">
+                        <li>Notification</li>
+                        <li>Notification</li>
+                        <li>Notification</li>
+                        <li>Notification</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
